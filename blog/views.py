4751390616d011bb9d03e7bc24d2152c44da2ce0
Vikil Naik <vikilnaik37@gmail.com>
2019-02-28 15:56:30 -0500

    Finished assignment

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def root(request):
 
 def home_page(request):
     context = {'client': 'A client is a piece of computer hardware or software that accesses a service made available by a server.',
-                'server': 'n computing, a server is a computer program or a device that provides functionality for other programs or devices, called "clients".',
+                'server': 'In computing, a server is a computer program or a device that provides functionality for other programs or devices, called "clients".',
                 'http_request': 'An HTTP client sends an HTTP request to a server in the form of a request message which includes following format',
                 'http_response': 'HTTP Response is the packet of information sent by Server to the Client in response to an earlier Request made by Client. ',
                 'django': 'Django is a Python-based free and open-source web framework, which follows the model-view-template architectural pattern. It is maintained by the Django Software Foundation, an independent organization established as a 501 non-profit. Djangos primary goal is to ease the creation of complex, database-driven websites'
