7c982769b6ad1dbf1f906e067ec0224abbc28f33
Vikil Naik <vikilnaik37@gmail.com>
2019-03-04 16:52:58 -0500

    When you click on the articles title it now takes you to the page of the actual article

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Article
 import datetime
 
@@ -12,4 +12,10 @@ def home_page(request):
     articles = Article.objects.filter(draft=False).order_by('-published_date').all()
     context = {'name': 'Vikil', 'day': date_now, 'articles': articles}
     response = render(request, 'index.html', context)
+    return HttpResponse(response)
+
+def article_page(request, id):
+    articles = get_object_or_404(Article, pk=id)
+    context = {'article': articles}
+    response = render(request, 'articles.html', context)
     return HttpResponse(response)
