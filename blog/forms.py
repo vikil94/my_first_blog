05c94e56687e97f4353ff7fa0a5edd9703053599
Vikil Naik <vikilnaik37@gmail.com>
2019-03-11 17:06:42 -0400

    Finished part 6 of this assignment

--- blog/forms.py
+++ blog/forms.py
@@ -15,19 +15,19 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'body', 'draft', 'published_date', 'author']
-
-    def clean(self):
-        super().clean()
-        description = self.cleaned_data.get('body')
-        draft = self.cleaned_data.get('draft')
-        published = self.cleaned_data.get('published_date')
-        today = datetime.date.today()
-        if len(description) <= 1:
-            raise ValidationError('The body must contain more then 1 character.')
-        if draft == True and published < today:
-            raise ValidationError('If draft is selected the published date must be in the future.')
-        if draft == False and published > today:
-            raise ValidationError('Published date cannot be a future date.')
+    # 
+    # def clean(self):
+    #     super().clean()
+    #     description = self.cleaned_data.get('body')
+    #     draft = self.cleaned_data.get('draft')
+    #     published = self.cleaned_data.get('published_date')
+    #     today = datetime.date.today()
+    #     if len(description) <= 1:
+    #         raise ValidationError('The body must contain more then 1 character.')
+    #     if draft = True and published < today:
+    #         raise ValidationError('If draft is selected the published date must be in the future.')
+    #     if draft = False and published > today:
+    #         raise ValidationError('Published date cannot be a future date.')
 
 
 class LoginForm(Form):
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import datetime, date
 from django.contrib.auth.models import User
 
 
